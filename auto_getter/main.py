a2ea3ad8ccda26cf52cc85a95a3183f55c6586c7
Shuery <shuery.shuai@outlook.com>
2022-03-25 19:14:21 +0800

    修复：配置文件中 `proxy-groups` 未同步更改。

--- auto_getter/main.py
+++ auto_getter/main.py
@@ -81,36 +81,62 @@ def determine_dict_in_another(dict_included, dict_including):
     return dict_included_set.issubset(dict_including_set)
 
 
-def rm_proxies_with_ciphers(proxies, ciphers):
+def rm_proxies_with_ciphers(profile_data, ciphers):
     """移除含有特定加密方式的代理。
 
-    :param proxies: 列表：需处理的代理。
+    :param profile_data: 字典：需处理的代理文件数据。
     :param ciphers: 列表：需移除的加密方式。
-    :return: 列表：移除加密方式后的代理。
+    :return: 字典：移除加密方式后的代理文件数据。
     """
     print('Removing proxies with "{ciphers}"...'.format(ciphers=ciphers))
+    proxies = profile_data['proxies']
+    proxy_groups = profile_data['proxy-groups']
     for proxy in proxies:
-        index = proxies.index(proxy)
+        proxy_index = proxies.index(proxy)
         if 'cipher' in proxy:
             for cipher in ciphers:
                 if proxy['cipher'] == cipher:
                     print('Found a proxy "No.{index}: {server}:{port}" has cipher: "{cipher}".'.format(
-                        index=index, server=proxy['server'], port=proxy['port'], cipher=cipher))
+                        index=proxy_index, server=proxy['server'], port=proxy['port'], cipher=cipher))
                     print('Now removing it...')
-                    del proxies[index]
+                    proxy_groups = rm_proxy_groups_proxies(proxy_groups, proxy['name'])
+                    profile_data['proxy-groups'] = proxy_groups
+                    del proxies[proxy_index]
+                    profile_data['proxies'] = proxies
+                else:
+                    print('The proxy "No.{index}: {server}:{port}" has no cipher: "{cipher}".'.format(
+                        index=proxy_index, server=proxy['server'], port=proxy['port'], cipher=cipher))
         else:
-            print('The proxy "No.{index}: {server}:{port}" has no cipher: "{cipher}".'.format(
-                index=index, server=proxy['server'], port=proxy['port'], cipher=cipher))
-    return proxies
+            print('The proxy "No.{index}: {server}:{port}" has no param cipher.'.format(
+                    index=proxy_index, server=proxy['server'], port=proxy['port']))
+    return profile_data
 
 
-def rm_outdated_proxies(proxies):
+def rm_proxy_groups_proxies(proxy_groups, proxy_name):
+    """移除代理组中的代理。
+
+    :param proxy_groups: 列表：需移除代理的代理组。
+    :param proxy_name: 字符串：移除代理依据的代理名。
+    :return: 列表：移除代理后的代理组。
+    """
+    for proxy_group in proxy_groups:
+        proxy_group_index = proxy_groups.index(proxy_group)
+        for proxy_group_proxy in proxy_group['proxies']:
+            proxy_group_proxy_index = proxy_group['proxies'].index(proxy_group_proxy)
+            if proxy_group_proxy == proxy_name:
+                del proxy_groups[proxy_group_index]['proxies'][proxy_group_proxy_index]
+    return proxy_groups
+
+
+def rm_outdated_proxies(profile_data):
     """移除过时代理。
 
-    :param proxies: 列表：需处理的代理。
-    :return: 列表：移除过时代理后的代理。
+    :param profile_data: 字典：需处理的代理配置文件数据。
+    :return: 字典：移除过时代理后的代理配置文件数据。
     """
     print('Removing outdated proxies...')
+    proxies = profile_data['proxies']
+    proxy_groups = profile_data['proxy-groups']
     proxies_servers = []
     for proxy in proxies:
         checking_index = proxies.index(proxy)
@@ -122,22 +148,26 @@ def rm_outdated_proxies(proxies):
                 print('Found a outdated proxy: No.{index}: "{server}:{port}".'.format(
                     index=existed_index + 1, server=proxy['server'], port=proxy['port']))
                 print('Now replacing it with the latest one...')
-                proxies[existed_index] = proxies[checking_index]
-                del proxies[checking_index]
+                proxy_groups = rm_proxy_groups_proxies(proxy_groups, proxies[existed_index]['name'])
+                profile_data['proxy-groups'] = proxy_groups
+                del proxies[existed_index]
+                profile_data['proxies'] = proxies
         proxies_servers.append({'checking_index': checking_index, 'server': proxy['server'], 'port': proxy['port']})
     print('Outdated proxies has been successfully removed!')
-    return proxies
+    return profile_data
 
 
-def rename_proxies(proxies):
+def rename_proxies(profile_data):
     """对代理进行重命名。
 
-    :param proxies: 列表：需处理的代理。
-    :return: 列表：重命名后的代理。
+    :param profile_data: 字典：需处理的代理文件数据。
+    :return: 字典：重命名后的代理配置文件数据。
     """
     print('Renaming proxies...')
+    proxies = profile_data['proxies']
+    proxy_groups = profile_data['proxy-groups']
     for proxy in proxies:
-        index = proxies.index(proxy)
+        proxy_index = proxies.index(proxy)
         # 使用 http://ip-api.com 的 API 进行服务器信息查询。
         print('Getting No.{index}: "{server}" information...'.format(index=index, server=proxy['server']))
         ip_api_link = 'http://ip-api.com/json/' + proxy['server']
@@ -150,13 +180,15 @@ def rename_proxies(proxies):
         if 'country' not in ip_info:
             print('No such server: "{server}.'.format(server=proxy['server']))
             print('Now removing it...')
-            del proxies[index]
+            proxy_groups = rm_proxy_groups_proxies(proxy_groups, proxies[proxy_index]['name'])
+            del proxies[proxy_index]
             continue
         # 根据获取的信息更改代理名。
         country = ip_info.get('country')
         city = ip_info.get('city')
         flag = emoji.emojize(':' + country + ':')
-        if city == 'Hong Kong' or city == 'Taiwan' or city == 'Macao':
+        if country == 'Hong Kong' or country == 'Taiwan' or country == 'Macao':
+            city = country
             country = 'China'
             flag = emoji.emojize(':' + country + ': :' + city + ':')
         if country == city:
@@ -164,15 +196,22 @@ def rename_proxies(proxies):
         else:
             position = country + ' ' + city
         # position = translate(position, 'en', 'zh-cn')
-        name = '{flag} {position} {index}'.format(flag=flag, position=position, index=index)
-        print(f'The server name is {name}.'.format(name=name))
-        proxies[index]['name'] = name
+        name = '{flag} {position} {index}'.format(flag=flag, position=position, index=proxy_index)
+        for proxy_group in proxy_groups:
+            proxy_group_index = proxy_groups.index(proxy_group)
+            for proxy_group_proxy in proxy_group['proxies']:
+                proxy_group_proxy_index = proxy_group['proxies'].index(proxy_group_proxy)
+                if proxy_group_proxy == proxy['name']:
+                    proxy_groups[proxy_group_index]['proxies'][proxy_group_proxy_index] = name
+        proxies[proxy_index]['name'] = name
+        profile_data['proxies'] = proxies
+        profile_data['proxy-groups'] = proxy_groups
         sleep(3)
-        if index % 3 == 0:
+        if proxy_index % 3 == 0:
             print('Sleeping for 5 seconds...')
             sleep(5)
     print('Proxies has been successfully renamed!')
-    return proxies
+    return profile_data
 
 
 def sort_dict_list(dict_list, dict_keys):
@@ -444,12 +483,18 @@ def get_profile(config_path):
             print('Profile "' + profile_name + '" download complete!')
             # 对下载的配置文件进行操作。
             profile_data = load_yaml_data(profile_path, not_supported_yaml_tags)
+            profile_data = rm_proxies_with_ciphers(profile_data, clash_not_supported_ciphers)
+            profile_data = rm_outdated_proxies(profile_data)
+            profile_data = rename_proxies(profile_data)
             proxies = profile_data['proxies']
-            proxies = rm_proxies_with_ciphers(proxies, clash_not_supported_ciphers)
-            proxies = rm_outdated_proxies(proxies)
-            proxies = rename_proxies(proxies)
+            proxy_groups = profile_data['proxy-groups']
             proxies = sort_dict_list(proxies, ['name'])
+            for proxy_group in proxy_groups:
+                proxy_group_index = proxy_groups.index(proxy_group)
+                proxy_group['proxies'].sort()
+                proxy_groups[proxy_group_index] = proxy_group
             profile_data['proxies'] = proxies
+            profile_data['proxy-groups'] = proxy_groups
             save_yaml_file(profile_data, profile_path)
             print('Profile "' + profile_name + '" update complete!')
         else:
--- auto_getter/test.py
+++ auto_getter/test.py
@@ -85,12 +85,30 @@ def determine_dict_in_another(dict_included, dict_including):
     return dict_a_set.issubset(dict_b_set)
 
 
-def rm_outdated_proxies(proxies):
+def rm_proxy_groups_proxies(proxy_groups, proxy_name):
+    """移除代理组中的代理。
+
+    :param proxy_groups: 列表：需移除代理的代理组。
+    :param proxy_name: 字符串：移除代理依据的代理名。
+    :return: 列表：移除代理后的代理组。
+    """
+    for proxy_group in proxy_groups:
+        proxy_group_index = proxy_groups.index(proxy_group)
+        for proxy_group_proxy in proxy_group['proxies']:
+            proxy_group_proxy_index = proxy_group['proxies'].index(proxy_group_proxy)
+            if proxy_group_proxy == proxy_name:
+                del proxy_groups[proxy_group_index]['proxies'][proxy_group_proxy_index]
+    return proxy_groups
+
+
+def rm_outdated_proxies(profile_data):
     """移除过时代理。
 
-    :param proxies: 列表：需处理的代理。
-    :return: 列表：移除过时代理后的代理。
+    :param profile_data: 字典：需处理的代理。
+    :return: 字典：移除过时代理后的代理文件数据。
     """
+    proxies = profile_data['proxies']
+    proxy_groups = profile_data['proxy-groups']
     proxies_servers = []
     for proxy in proxies:
         checking_index = proxies.index(proxy)
@@ -102,44 +120,56 @@ def rm_outdated_proxies(proxies):
                 print('Found a outdated proxy: No.{index}: "{server}:{port}".'.format(
                     index=existed_index + 1, server=proxy['server'], port=proxy['port']))
                 print('Now replacing it with the latest one...')
-                proxies[existed_index] = proxies[checking_index]
-                del proxies[checking_index]
+                proxy_groups = rm_proxy_groups_proxies(proxy_groups, proxies[existed_index]['name'])
+                profile_data['proxy-groups'] = proxy_groups
+                del proxies[existed_index]
+                profile_data['proxies'] = proxies
         proxies_servers.append({'checking_index': checking_index, 'server': proxy['server'], 'port': proxy['port']})
-    return proxies
+    return profile_data
 
 
-def rm_proxies_with_ciphers(proxies, ciphers):
+def rm_proxies_with_ciphers(profile_data, ciphers):
     """移除含有特定加密方式的代理。
 
-    :param proxies: 列表：需处理的代理。
+    :param profile_data: 字典：需处理的代理文件数据。
     :param ciphers: 列表：需移除的加密方式。
-    :return: 列表：移除加密方式后的代理。
+    :return: 字典：移除加密方式后的代理文件数据。
     """
     print('Removing proxies with "{ciphers}"...'.format(ciphers=ciphers))
+    proxies = profile_data['proxies']
+    proxy_groups = profile_data['proxy-groups']
     for proxy in proxies:
-        index = proxies.index(proxy)
+        proxy_index = proxies.index(proxy)
         if 'cipher' in proxy:
             for cipher in ciphers:
                 if proxy['cipher'] == cipher:
                     print('Found a proxy "No.{index}: {server}:{port}" has cipher: "{cipher}".'.format(
-                        index=index, server=proxy['server'], port=proxy['port'], cipher=cipher))
+                        index=proxy_index, server=proxy['server'], port=proxy['port'], cipher=cipher))
                     print('Now removing it...')
-                    del proxies[index]
+                    proxy_groups = rm_proxy_groups_proxies(proxy_groups, proxy['name'])
+                    profile_data['proxy-groups'] = proxy_groups
+                    del proxies[proxy_index]
+                    profile_data['proxies'] = proxies
+                else:
+                    print('The proxy "No.{index}: {server}:{port}" has no cipher: "{cipher}".'.format(
+                        index=proxy_index, server=proxy['server'], port=proxy['port'], cipher=cipher))
         else:
-            print('The proxy "No.{index}: {server}:{port}" has no cipher: "{cipher}".'.format(
-                        index=index, server=proxy['server'], port=proxy['port'], cipher=cipher))
-    return proxies
+            print('The proxy "No.{index}: {server}:{port}" has no param cipher.'.format(
+                    index=proxy_index, server=proxy['server'], port=proxy['port']))
+    return profile_data
 
 
-def rename_proxies(proxies):
+def rename_proxies(profile_data):
     """对代理进行重命名。
 
-    :param proxies: 列表：需处理的代理。
-    :return: 列表：重命名后的代理。
+    :param profile_data: 字典：需处理的代理文件数据。
+    :return: 字典：重命名后的代理配置文件数据。
     """
     print('Renaming proxies...')
+    proxies = profile_data['proxies']
+    proxy_groups = profile_data['proxy-groups']
     for proxy in proxies:
-        index = proxies.index(proxy)
+        proxy_index = proxies.index(proxy)
         # 使用 http://ip-api.com 的 API 进行 IP 信息查询。
         ip_api_link = 'http://ip-api.com/json/' + proxy['server']
         resp = urlopen(ip_api_link)
@@ -150,17 +180,38 @@ def rename_proxies(proxies):
         if 'country' not in ip_info:
             print('No such server: "{server}.'.format(server=proxy['server']))
             print('Now removing it...')
-            del proxies[index]
+            proxy_groups = rm_proxy_groups_proxies(proxy_groups, proxies[proxy_index]['name'])
+            del proxies[proxy_index]
             continue
         country = ip_info.get('country')
         city = ip_info.get('city')
         flag = emoji.emojize(':' + country + ':')
+        if country == 'Hong Kong' or country == 'Taiwan' or country == 'Macao':
+            city = country
+            country = 'China'
+            flag = emoji.emojize(':' + country + ': :' + city + ':')
+        if country == city:
+            position = country
+        else:
+            position = country + ' ' + city
         position = country + ' ' + city
         # position = translate(position, 'en', 'zh-cn')
-        name = '{flag} {position} {index}'.format(flag=flag, position=position, index=index)
-        proxies[index]['name'] = name
+        name = '{flag} {position} {index}'.format(flag=flag, position=position, index=proxy_index)
+        for proxy_group in proxy_groups:
+            proxy_group_index = proxy_groups.index(proxy_group)
+            for proxy_group_proxy in proxy_group['proxies']:
+                proxy_group_proxy_index = proxy_group['proxies'].index(proxy_group_proxy)
+                if proxy_group_proxy == proxy['name']:
+                    proxy_groups[proxy_group_index]['proxies'][proxy_group_proxy_index] = name
+        proxies[proxy_index]['name'] = name
+        profile_data['proxies'] = proxies
+        profile_data['proxy-groups'] = proxy_groups
         sleep(3)
-    return proxies
+        if proxy_index % 3 == 0:
+            print('Sleeping for 5 seconds...')
+            sleep(5)
+    print('Proxies has been successfully renamed!')
+    return profile_data
 
 
 def main():
@@ -170,17 +221,23 @@ def main():
     directories_config = others_config['directories']
     not_supported_yaml_tags = others_config['not-supported-yaml-tags']
     profiles_stored_dir = directories_config['profiles-stored-dir']
-    profile_path = profiles_stored_dir + '/Public from FQD.yml'
+    profile_path = profiles_stored_dir + '/test.yml'
     profile_config = config['profile']
     profile_clash_config = profile_config['clash']
     clash_not_supported_ciphers = profile_clash_config['not-supported-ciphers']
     profile_data = load_yaml_data(profile_path, not_supported_yaml_tags)
+    profile_data = rm_proxies_with_ciphers(profile_data, clash_not_supported_ciphers)
+    profile_data = rm_outdated_proxies(profile_data)
+    profile_data = rename_proxies(profile_data)
     proxies = profile_data['proxies']
-    proxies = rm_proxies_with_ciphers(proxies, clash_not_supported_ciphers)
-    proxies = rm_outdated_proxies(proxies)
-    proxies = rename_proxies(proxies)
+    proxy_groups = profile_data['proxy-groups']
     proxies = sort_dict_list(proxies, ['name'])
+    for proxy_group in proxy_groups:
+        proxy_group_index = proxy_groups.index(proxy_group)
+        proxy_group['proxies'].sort()
+        proxy_groups[proxy_group_index] = proxy_group
     profile_data['proxies'] = proxies
+    profile_data['proxy-groups'] = proxy_groups
     save_yaml_file(profile_data, profile_path)
 
 
