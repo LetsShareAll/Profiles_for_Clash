def8ad4d91c009e33b7d2dced169e12504e506cb
Shuery <2463253700@qq.com>
2022-11-29 17:48:42 +0800

    添加生成虚拟环境的命令行。

--- run.ps1
+++ run.ps1
@@ -1,3 +1,4 @@
+python -m venv ./venv
 ./venv/Scripts/activate.ps1
 cd ./auto_getter
 pip3 install -r requirements.txt
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+python -m venv ./venv
 source ./venv/Scripts/activate
 cd ./auto_getter || exit
 pip install -r requirements.txt
