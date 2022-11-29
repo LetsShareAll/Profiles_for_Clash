b18c5ed8834dea71638cc60b4937d307a2fb1baa
Shuery <2463253700@qq.com>
2022-11-30 00:01:34 +0800

    更改 `python3`、`pip3` 为 `python`、`pip`。

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 python -m venv ./venv
 ./venv/Scripts/activate.ps1
 Set-Location ./auto_getter
-pip3 install -r requirements.txt
-python3 ./main.py
+pip install -r requirements.txt
+python ./main.py
